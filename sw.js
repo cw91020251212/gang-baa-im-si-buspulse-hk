5aa984d8831d07a3dc325bf982089697df6ca8ac
cw91020251212 <2.49233253e+08+cw91020251212@users.noreply.github.com>
2026-09-23 10:23:51 +0000

    ui: match map pin control border styling

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* BusPulse HK service worker: app shell cache and authorized local notifications only. */
-const SHELL = 'buspulse-hk-shell-v51-getoff-width-red-map-border';
+const SHELL = 'buspulse-hk-shell-v52-match-control-border';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './fare-index.json', './place-index.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(SHELL).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
