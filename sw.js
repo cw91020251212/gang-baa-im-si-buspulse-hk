955bae5905d8af9a2ed0d55b5d633e776bfe3427
cw91020251212 <2.49233253e+08+cw91020251212@users.noreply.github.com>
2026-09-23 10:58:48 +0000

    ui: slim and gray route outline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* BusPulse HK service worker: app shell cache and authorized local notifications only. */
-const SHELL = 'buspulse-hk-shell-v53-map-controls-refined';
+const SHELL = 'buspulse-hk-shell-v54-route-outline';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './fare-index.json', './place-index.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(SHELL).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
