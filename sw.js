5c8fb5d9bfc4bc50acaa8ffcdb18cf7e325de811
cw91020251212 <2.49233253e+08+cw91020251212@users.noreply.github.com>
2026-09-23 13:44:35 +0000

    ui: increase stop marker blue contrast

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* BusPulse HK service worker: app shell cache and authorized local notifications only. */
-const SHELL = 'buspulse-hk-shell-v60-clear-color-map';
+const SHELL = 'buspulse-hk-shell-v61-deeper-blue-stops';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './fare-index.json', './place-index.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(SHELL).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
