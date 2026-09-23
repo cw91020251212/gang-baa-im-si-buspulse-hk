eedd807e82b3e04ce4100d72440ab80b01b55f43
cw91020251212 <2.49233253e+08+cw91020251212@users.noreply.github.com>
2026-09-23 09:39:38 +0000

    ui: use light gray GPS map control

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* BusPulse HK service worker: app shell cache and authorized local notifications only. */
-const SHELL = 'buspulse-hk-shell-v46-gps-button-contrast';
+const SHELL = 'buspulse-hk-shell-v47-gps-light-button';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './fare-index.json', './place-index.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(SHELL).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
