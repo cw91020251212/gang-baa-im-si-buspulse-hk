cdbaebb2bb59f04a78f43f7c407850eff513efd0
cw91020251212 <2.49233253e+08+cw91020251212@users.noreply.github.com>
2026-09-23 18:27:14 +0000

    ui: enlarge get-off label and clear map button

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* BusPulse HK service worker: app shell cache and authorized local notifications only. */
-const SHELL = 'buspulse-hk-shell-v69-black-map-and-stable-stop-center';
+const SHELL = 'buspulse-hk-shell-v70-large-getoff-transparent-map';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './fare-index.json', './place-index.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(SHELL).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
