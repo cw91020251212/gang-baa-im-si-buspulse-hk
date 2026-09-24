8e1506231022e185bd427b9712ccf6c209aacede
cw91020251212 <2.49233253e+08+cw91020251212@users.noreply.github.com>
2026-09-24 03:06:58 +0000

    chore: bump cache for district place matching

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* BusPulse HK service worker: app shell cache and authorized local notifications only. */
-const SHELL = 'buspulse-hk-shell-v75-place-match-tai-wo-hau';
+const SHELL = 'buspulse-hk-shell-v76-place-match-taikoo-shing';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './fare-index.json', './place-index.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(SHELL).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
