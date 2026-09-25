bc4492017992d7a67e8f8aeab8b9dee9d005c963
cw91020251212 <2.49233253e+08+cw91020251212@users.noreply.github.com>
2026-09-25 01:08:32 +0000

    fix: honor custom trip destination

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* BusPulse HK service worker: app shell cache and authorized local notifications only. */
-const SHELL = 'buspulse-hk-shell-v78-transfer-distance-search';
+const SHELL = 'buspulse-hk-shell-v79-custom-trip-destination';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './fare-index.json', './place-index.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(SHELL).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
